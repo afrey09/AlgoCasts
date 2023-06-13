124dd235b8df81331e5f0d92c2c7716a3ce6bd1e
Adrienne Frey <AdrienneN.Frey@gmail.com>
2023-06-13 16:28:33 -0700

    fizzbuzz solution

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,26 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+    
+        for (let i = 1; i <= n; i++) {
+            // is the number a multiple of 3 and 5?
+            if (i % 3 === 0 && i % 5 === 0) {
+                console.log('fizzbuzz');
+            } 
+            // is the number a multiple of 3?
+            else if (i % 3 === 0) {
+                console.log('fizz');
+            } 
+            // is the number a multiple of 5?
+            else if (i % 5 === 0) {
+                console.log('buzz');
+            } 
+            // if none of the above, just print the number
+            else {
+                console.log(i);
+            }
+        } 
+}
 
 module.exports = fizzBuzz;
