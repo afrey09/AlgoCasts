158ac7f004f33bc24a156f6ffa7fd60454dc209c
Adrienne Frey <AdrienneN.Frey@gmail.com>
2023-06-27 09:36:37 -0700

    chunked solution using slice

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -10,14 +10,24 @@
 
 function chunk(array, size) {
     const chunked = [];
-    for (let element of array) {
-      const last = chunked[chunked.length - 1];
-        if (!last || last.length === size) {
-            chunked.push([element]);
-        } else {
-            last.push(element);
-        }
+    let index = 0;
+    
+    while (index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
     }
+
     return chunked;
 }
     module.exports = chunk;
+
+    // const chunked = [];
+    // for (let element of array) {
+    //   const last = chunked[chunked.length - 1];
+    //     if (!last || last.length === size) {
+    //         chunked.push([element]);
+    //     } else {
+    //         last.push(element);
+    //     }
+    // }
+    // return chunked;
